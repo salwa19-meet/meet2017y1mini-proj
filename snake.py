dd88720fd5c89f4fb78ac4bb931ad7d982b2bb3b
salwa19-meet <salwa19@meet.mit.edu>
2017-08-01 11:25:51 +0200

    Stopping here

---
+++ snake.py
@@ -0,0 +1,20 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: /home/meet/salwa19_mini-proj/meet2017y1mini-proj/snake.py =====
+Traceback (most recent call last):
+  File "/home/meet/salwa19_mini-proj/meet2017y1mini-proj/snake.py", line 6, in <module>
+    tuetle.setup(size_x, size_y)
+NameError: name 'tuetle' is not defined
+>>> 
+===== RESTART: /home/meet/salwa19_mini-proj/meet2017y1mini-proj/snake.py =====
+>>> 
+===== RESTART: /home/meet/salwa19_mini-proj/meet2017y1mini-proj/snake.py =====
+>>> pos_list
+[(20.0, 0.0), (40.0, 0.0), (60.0, 0.0), (80.0, 0.0), (100.0, 0.0), (120.0, 0.0), (140.0, 0.0), (160.0, 0.0), (180.0, 0.0), (200.0, 0.0)]
+>>> stamp_list
+[8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+>>> 
+===== RESTART: /home/meet/salwa19_mini-proj/meet2017y1mini-proj/snake.py =====
+>>> 
